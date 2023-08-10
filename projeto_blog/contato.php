8e772fe57ba8be913b874578e4d0ca593c8dda4c
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-08-10 11:07:01 -0300

    Delete

--- projeto_blog/contato.php
+++
@@ -1,3 +0,0 @@
-<?php include_once('partes/header.php')?>
-    <h1>Contato</h1>
-<?php include_once('partes/footer.php')?>
--- projeto_blog/partes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <p>Hora de codar &copy; 2023</p>
-</footer>
-</body>
-</html>
